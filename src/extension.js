186db2f089adb02b6725eeb527c4cbf84f9f0e18
Glen Chiacchieri <www@glench.com>
2019-10-09 07:48:21 -0400

    progress

--- src/extension.js
+++ src/extension.js
@@ -1,6 +1,5 @@
 "use strict";
 
-console.log("Extension loading...");
 const jQuery = require("jquery");
 const $ = jQuery;
 const GmailFactory = require("gmail-js");
@@ -8,12 +7,33 @@ const gmail = new GmailFactory.Gmail($);
 window.gmail = gmail;
 
 gmail.observe.on("load", () => {
-    const userEmail = gmail.get.user_email();
-    console.log("Hello, " + userEmail + ". This is your extension talking!");
-
-    gmail.observe.on("view_email", (domEmail) => {
-        console.log("Looking at email:", domEmail);
-        const emailData = gmail.new.get.email_data(domEmail);
-        console.log("Email data:", emailData);
-    });
+    if (gmail.check.is_inside_email()) {
+        const id = gmail.get.email_id()
+        const email = new gmail.dom.email(id)
+        console.log(id, email)
+        modify_email(email)
+    }
+    gmail.observe.on("view_email", modify_email)
 });
+
+const separator_re = /(<br>|,|-|at|from|\:|\s)+/gi
+const day_re = /(monday|tuesday|wednesday|thursday|friday|saturday|sunday|mon|tue|wed|thurs|thur|fri|sat|sun)\.?/gi
+// date_re testing: https://regexr.com/4meog
+const date_re = /(january|february|march|april|may|june|july|august|september|october|november|december|jan|feb|mar|apr|jun|aug|sept|sep|oct|nov|dec).?\s+([0-3]?[0-9])(st|nd|rd|th)?,?\s*(\d{4})?/gi
+// time_re testing: https://regexr.com/4meoa
+const time_re = /(([0-9][0-9]?)(:([0-5][0-9]))?\s*([paPA]\.?[mM]\.?)?)|midnight|noon/gi
+
+const day_date_time_re = new RegExp(`(${day_re.source})?(${separator_re.source})*(${date_re.source})(${separator_re.source})*(${time_re.source})`, 'gi')
+
+function modify_email(email) {
+    // test with this email: https://mail.google.com/mail/u/0/#inbox/FMfcgxwDrcBVgdSXpwJSntsSZLWmBvzn
+
+    console.log("Email body:", email.body());
+
+    console.log(day_date_time_re)
+    email.body(email.body().replace(day_date_time_re, 'SDFDSFDSFDSF'))
+}
+
+function make_gcal_link() {
+    return '<a href="" target="_blank" style="">   </a>'
+}
